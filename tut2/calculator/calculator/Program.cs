b46918c287615ebfd22b67c0bc10e4ddfb430cea
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-17 13:21:44 +0530

    Simple calculation

---
+++ tut2/calculator/calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+     public class Program
+     {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+            Console.WriteLine(a + b);
+            Console.WriteLine($"Addtion is {a + b}");
+            Console.WriteLine($"Substration is {a - b}");
+            Console.WriteLine($"Multiplaction is {a * b}");
+            Console.WriteLine($"Division is {a / b}");
+            Console.ReadKey();
+        }
+    }
+}
