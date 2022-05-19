a58ad36c83218db5ea2adebf4251f1cb6d00ee81
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-19 07:52:42 +0530

    Program to show the use of BackgroundColor, ForegroundColor, and Title properties of Console class in C#.

---
+++ tut9/About color/About color/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace About_color
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = "Understanding Console Class";
+            Console.WriteLine("BackgroundColor Blue");
+            Console.WriteLine("ForegroundColor White");
+            Console.WriteLine("Title Understanding Console Class");
+            Console.ReadKey();
+        }
+    }
+}
