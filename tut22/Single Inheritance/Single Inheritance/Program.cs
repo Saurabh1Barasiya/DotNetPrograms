ccb5293e22bbb357efc6465f7dee624de3c128e7
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-05-31 14:04:14 +0530

    Single Inheritance

---
+++ tut22/Single Inheritance/Single Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Inheritance
+{
+
+    // In single inheritance we have only 2 class , Parent and child class.
+    // child class acess all the member of parent class.
+    // : is the symbol of inheritance in C#.
+
+
+    class Parent
+    {
+        public void Show1()
+        {
+            Console.WriteLine("Parent class show method");
+        }
+    }
+    class Child:Parent 
+    {
+        public void Show2()
+        {
+            Console.WriteLine("Child class show method");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating child class object.
+            Child obj = new Child();
+            obj.Show1();
+            obj.Show2();
+            Console.ReadKey();
+
+        }
+    }
+}
