f22308fb68326ef720180a1d95ec2720c695db14
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-06-01 14:40:53 +0530

    Auto implemented properties

---
+++ tut29/Auto implemented properties/Auto implemented properties/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_implemented_properties
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Id = 1;
+            student.Name = "saurabh";
+            student.Age = 23;
+            student.Address = "Harrai";
+
+            Console.WriteLine(student.Id);
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Age);
+            Console.WriteLine(student.Address);
+
+            Console.ReadKey();
+        }
+    }
+}
