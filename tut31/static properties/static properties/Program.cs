e1dbea1498b5d62d6f8df68c4477d2142b45876d
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-06-01 15:53:41 +0530

    Static properties

---
+++ tut31/static properties/static properties/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_properties
+{
+    class Student
+    {
+        public static string Name { get; set; }
+        public static int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.Age = 50;
+            Student.Name = "Harshit";
+
+            Console.WriteLine(Student.Age);
+            Console.WriteLine(Student.Name);
+
+            Console.ReadKey();
+        }
+    }
+}
