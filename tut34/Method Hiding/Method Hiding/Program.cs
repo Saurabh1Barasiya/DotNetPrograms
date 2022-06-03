360e62585470fd2958e836de7946f833ad57fa31
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-06-03 16:28:20 +0530

    method hiding

---
+++ tut34/Method Hiding/Method Hiding/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Hiding
+{
+
+    // Method hiding yaha ham new keyword ka use karege.
+    // if new na likhe to warning aata h. but new likhna matlab ham forcefully bol rahe h ki ham method hiding kar rahe h. 
+    class Parent
+    {
+        public void show()
+        {
+            Console.WriteLine("Parent class method");
+        }
+    }
+    class Child : Parent
+    {
+        public new void show()
+        {
+            // base.show(); base means parent class ka function chalega.
+            Console.WriteLine("Child class method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parent parent = new Parent();
+            //parent.show();
+
+            //Child child = new Child();
+            //child.show();
+
+            Child child = new Child();
+
+            ((Parent)child).show(); // type conversion. child ka object parent me convert ho gaya h.
+
+            Console.ReadKey();
+        }
+    }
+}
