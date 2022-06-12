a2c0ab8bbf0e001e337c1c0076e0e31ebb969848
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-06-12 13:10:21 +0530

    More  concept of ananomous methods.

---
+++ tut41/Ananomous function more concept/Ananomous function more concept/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ananomous_function_more_concept
+{
+    public delegate void MyDelegate(int a);
+    internal class Program
+    {
+        public static void CallDelegate(MyDelegate del,int a)
+        {
+            a++;
+            del(a);
+
+        }
+        static void Main(string[] args)
+        {
+            Program.CallDelegate(delegate(int b){
+                b = b + 10;
+                Console.WriteLine(b);
+            },5);
+            Console.ReadKey();
+        }
+    }
+}
