fdfe11d251ea3d6a3b9b4a205a3e2f8c665c39a9
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-06-17 15:28:31 +0530

    hashtable

---
+++ tut53/about hashmap/about hashmap/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace about_hashmap
+{
+    // Hashtable uses key-value pair.
+    // yaha order nahi hota h in according to me.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable  hashtable = new Hashtable();
+            hashtable.Add("Id", 12);
+            hashtable.Add("Name", "saurabh");
+            hashtable.Add("age", 22);
+            hashtable.Add("Address", "Haraai");
+            hashtable.Add("Salary", 24000);
+
+            foreach (var item in hashtable.Keys)
+            {
+                Console.WriteLine(item+" "+hashtable[item]);
+            }
+
+
+            Hashtable hashtable1 = new Hashtable()
+            {
+                {"Id", 123 },
+                {"Name", "peeyush" },
+                {"age", 23 },
+                { "Address", "Haraai"},
+                {"Salary", 28000 }
+            };
+            foreach (var item in hashtable.Keys)
+            {
+                Console.WriteLine(item + " " + hashtable1[item]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
