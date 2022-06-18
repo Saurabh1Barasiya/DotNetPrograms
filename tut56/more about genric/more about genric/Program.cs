59b5ef66ea02076364e8fbd4cdeb0de1ab120525
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-06-18 15:43:56 +0530

    2nd about genricks

---
+++ tut56/more about genric/more about genric/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace more_about_genric
+{
+    class MygenricClass<UnknowData>
+    {
+        public UnknowData Check(UnknowData a, UnknowData b)
+        {
+            if (a.Equals(b))
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MygenricClass<int> mygenricClass = new MygenricClass<int>();
+            Console.WriteLine(mygenricClass.Check(5,5));
+            Console.ReadKey();
+        }
+    }
+}
